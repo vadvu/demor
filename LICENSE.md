c0c6fd3e83549e52603212ae32e3d724e1a8b454
Vadim Ustyuzhanin <vadim.v.ustyuzhanin@gmail.com>
2025-08-11 16:22:58 +0300

    (*μ_μ)

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 # MIT License
 
-Copyright (c) 2023 demor authors
+Copyright (c) 2025 demor authors
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- R/hli.R
+++ R/hli.R
@@ -6,12 +6,15 @@
 #' @references Ghislandi, S., Sanderson, W.C., & Scherbov, S. (2019). A Simple Measure of Human Development: The Human Life Indicator. *Population and Development Review*, *45*, 219–233.
 #' @details
 #' It is calculated as
-#' \deqn{HLI = \prod_{x=\alpha}^{\omega}(x + a_x)^d_x}
-#' where \eqn{\alpha, \omega} are the first and last age groups, \eqn{x} is age, \eqn{a_x, d_x} are life table functions.
+#' \deqn{HLI = \prod_{x=\alpha}^{\omega}(x + a_x)^{d_x}}
+#' where \eqn{\alpha, \omega} are the first and last age groups, \eqn{x} is age, \eqn{a_x, d_x} are life table functions (s.t. \eqn{\sum_{x=\alpha}^{\omega} d_x = 1}).
 #' @return HLI value
 #' @export
 hli <- function(age, mx, ...){
   lt <- LT(age = age, mx = mx, ...)
+  if(round(sum(lt[,"dx"]), 2) > 1){
+    lt[,"dx"] <- lt[,"dx"] / sum(lt[,"dx"])
+  }
   val <- exp(sum(log(lt[,"age"] + lt[,"ax"])*lt[,"dx"]))
   return(val)
 }
